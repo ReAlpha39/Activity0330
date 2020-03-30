7143abe671917acc87fce4a8695185254f3762a2
YunusNS <yunusw718@gmail.com>
2020-03-30 14:32:54 +0800

    add method untuk pada option menu untuk berpindah activity

--- app/src/main/java/id/nyaaa/activity0330/Menu2.java
+++ app/src/main/java/id/nyaaa/activity0330/Menu2.java
@@ -2,7 +2,11 @@ package id.nyaaa.activity0330;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Menu2 extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class Menu2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu2);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.m1) {
+            startActivity(new Intent(this, Registrasi.class));
+        } else if (item.getItemId() == R.id.m2) {
+            startActivity(new Intent(this, Menu2.class));
+        } else if (item.getItemId() == R.id.m3) {
+            startActivity(new Intent(this, Menu3.class));
+        }
+
+        return true;
     }
 }
